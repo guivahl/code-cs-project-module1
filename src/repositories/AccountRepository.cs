a5d3db533a0b6a2a7c3a9afa34df7f79e8e9d1f4
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2022-12-27 18:54:55 -0300

    feat: account repository file

---
+++ src/repositories/AccountRepository.cs
@@ -0,0 +1,37 @@
+namespace src;
+
+public static class AccountRepository
+{
+    private static readonly int ACCOUNT_NUMBER_LENGTH = 5;
+    private static readonly int CHECK_DIGIT_LENGTH = 1;
+    private static List<Account> Accounts { get; } = new List<Account>();
+
+    private static bool AlreadyCreated(string accountNumber) => 
+        Accounts.Any(account => account.AccountNumber == accountNumber);
+
+    public static Account? Create() {
+        int tries = 0, MAX_TRIES = 10;
+
+        string accountNumber = Utils.RandomStringOfNumbers(ACCOUNT_NUMBER_LENGTH);
+        string checkDigit = Utils.RandomStringOfNumbers(CHECK_DIGIT_LENGTH);
+
+        while (AlreadyCreated(accountNumber) && tries < MAX_TRIES) {
+            accountNumber = Utils.RandomStringOfNumbers(ACCOUNT_NUMBER_LENGTH);        
+            tries++;
+        }
+
+        if (tries == MAX_TRIES) {
+            System.Console.WriteLine("Not possible to create new account");
+            return null;
+        }
+        
+        Account account = new Account(accountNumber, checkDigit);
+        
+        Accounts.Add(account);
+
+        return account;
+    }
+
+    public static Account? Find(string accountNumber) => 
+        Accounts.FirstOrDefault(account => account.AccountNumber == accountNumber);
+}
