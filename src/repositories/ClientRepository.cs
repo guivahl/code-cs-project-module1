c1b684954d7fd13ff8ad58c1cfcf75e793cf9029
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2022-12-27 18:56:09 -0300

    feat: client repository with brazilian utilities dll to deal with cpf

---
+++ src/repositories/ClientRepository.cs
@@ -0,0 +1,53 @@
+namespace src;
+
+using DanielMarques.Utilities;
+
+public static class ClientRepository
+{
+    private static readonly int CPF_LENGTH = 11;
+    public static List<Client> Clients { get; set; } = new List<Client>();
+    public static Client? FindByCpf(string clientCpf) => 
+        Clients.FirstOrDefault(client => client.CPF == clientCpf);
+
+    public static Client? Create(string clientCpf, string name) {
+        if (clientCpf.Length != CPF_LENGTH) {
+            System.Console.WriteLine("CPF deve possuir 11 digitos numericos");
+            return null;
+        }
+
+        CPF cpf = clientCpf;
+        
+        bool isValidCpf = cpf.IsValid();
+
+        if (!isValidCpf) {
+            System.Console.WriteLine("CPF inválido");
+            return null;
+        }
+
+        Client? alreadyRegistered = ClientRepository.FindByCpf(clientCpf);
+
+        if (alreadyRegistered != null) {
+            System.Console.WriteLine("Client already registered");
+            return null;
+        } 
+
+        Account? account = AccountRepository.Create();
+
+        if (account == null) return null;
+
+        Client client = new Client(clientCpf, name, account);
+
+        return client;
+    }
+
+    public static void Show(Client client) {
+        string clientInfo = $"Cliente: {client.Name}\nCPF: {client.CPF}\nSaldo: {client.Account.Balance}\n";
+
+        string accountInfo = $"Conta: {client.Account.AccountNumber}-{client.Account.CheckDigit}\nAgência: {client.Account.Agency}-{client.Account.CheckDigit}\n";
+        System.Console.WriteLine(String.Join("", clientInfo, accountInfo));
+    }
+    
+    public static void Update(Client client, string newName) => client.Name = newName;
+
+    public static void Deactivate(Client client) => client.DeactivateAt = new DateTime();
+}
--- src/utils/Utils.cs
+++ src/utils/Utils.cs
@@ -9,4 +9,11 @@ public class Utils
         return new string(Enumerable.Repeat(chars, length)
             .Select(s => s[random.Next(s.Length)]).ToArray());
     }
+
+    public static string RandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
 }
