21ba88571e176b44f5dc56d585764bb61bf0efca
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2022-12-28 16:50:25 -0300

    feat: file service class

---
+++ src/services/FileService.cs
@@ -0,0 +1,38 @@
+namespace src;
+
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+public class FileService
+{
+    private string Filepath { get; init; }
+    private CsvConfiguration csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
+    public FileService(string filename)
+    {
+        string pathDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        string filepath = Path.Join(pathDesktop, filename);
+
+        this.Filepath = filepath;
+    }
+    public bool FileExists() => new FileInfo(this.Filepath).Exists;
+
+    public List<T> Read<T>()
+    {
+        using StreamReader stream = new StreamReader(this.Filepath);
+        using CsvReader csv = new CsvReader(stream, CultureInfo.InvariantCulture);
+   
+        List<T> records = csv.GetRecords<T>().ToList();
+
+        return records;
+    }
+
+    public void Write<T>(List<T> records)
+    {
+        using StreamWriter stream = new StreamWriter(this.Filepath);
+        using CsvWriter csv = new CsvWriter(stream, CultureInfo.InvariantCulture);
+
+        csv.WriteRecords(records);
+   }
+}
