fbdd410641d555372a57f26834c32290a0c5e274
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2023-01-02 16:04:57 -0300

    feat: add console menu with employee and client options

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
 ﻿namespace src;
+using ConsoleTools;
+
 class Program
 {
     static void Main(string[] args)
@@ -13,5 +15,46 @@ class Program
             Console.ReadKey();
             Console.Clear();
         };
+
+        ConsoleMenu clientMenu = new ConsoleMenu()
+            .Add("Create new client", () => ClientService.Create())
+            .Add("Search client", () => ClientService.Find())
+            .Add("Update client", () => ClientService.Update())
+            .Add("Deactivate client", () => ClientService.Deactivate())
+            .Add("Close", ConsoleMenu.Close)
+            .Configure(config =>
+                    {
+                    config.Selector = ">>> ";
+                    config.Title = "Client Menu";
+                    config.EnableWriteTitle = true;
+                    config.EnableBreadcrumb = true;
+            });
+        
+        ConsoleMenu employeeMenu = new ConsoleMenu()
+            .Add("Create new employee", () => EmployeeService.Create())
+            .Add("Update employee password", () => EmployeeService.UpdatePassword())
+            .Add("Deactivate employee", () => EmployeeService.Deactivate())
+            .Add("Close", ConsoleMenu.Close)
+            .Configure(config =>
+                    {
+                    config.Selector = ">>> ";
+                    config.Title = "Employee Menu";
+                    config.EnableWriteTitle = true;
+                    config.EnableBreadcrumb = true;
+            });
+
+        ConsoleMenu menu = new ConsoleMenu()
+            .Add("Client", clientMenu.Show)
+            .Add("Employee", employeeMenu.Show)
+            .Add("Close", ConsoleMenu.Close)
+            .Configure(config =>
+                    {
+                    config.Selector = ">>> ";
+                    config.Title = "Main Menu";
+                    config.EnableWriteTitle = true;
+                    config.EnableBreadcrumb = true;
+            });
+
+        menu.Show();
     }
 }
